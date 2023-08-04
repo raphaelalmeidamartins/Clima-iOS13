6571477d391bf63c79d69fe5d451b396d97d6a57
Raphael Martins <64360469+raphaelalmeidamartins@users.noreply.github.com>
2023-08-03 22:01:55 -0300

    :sparkles: feat: customize UITextField keyboard and create action to print the text in the debug console

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -11,14 +11,19 @@ import UIKit
 class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
+    
     @IBOutlet weak var temperatureLabel: UILabel!
+    
     @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
 }
-
